6d1aca12f66a4f44c6badf24d8908de9af54a6b6
Unknown <kshekhar@uw.edu>
2018-08-02 16:45:28 -0700

    Working Qt exercise

--- krishnenduShekhar/Qt/helloWorld/mainwindow.cpp
+++ krishnenduShekhar/Qt/helloWorld/mainwindow.cpp
@@ -1,14 +1,68 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->statecomboBox->addItems(stateList);
+
+
+
+    ui->firstNamelineEdit->setText("Krish");
+    ui->lastNamelineEdit->setText("Shekhar");
+    ui->addresslineEdit->setText("2310 NE 48th St");
+    ui->citylineEdit->setText("Seattle");
+    ui->ZIPlineEdit->setText("98105");
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_savePushButton_clicked()
+{
+    gatherData();
+}
+
+void MainWindow::on_showPushButton_clicked()
+{
+    QString outputString = mydata.firstName+" "+mydata.lastName+"\n\n";
+
+    outputString.append("Address : ");
+    outputString.append(mydata.address);
+    outputString.append(", "+mydata.city +", " + mydata.state + ", "+ mydata.zip+"\n\n");
+
+    outputString.append("Date of Birth : ");
+    outputString.append(mydata.date.toString());
+    ui->outputtextBrowser->setText(outputString);
+
+    this->show();
+
+    QMessageBox Msgbox;
+    Msgbox.setText(outputString);
+    Msgbox.exec();
+
+
+}
+
+void MainWindow::gatherData(){
+    mydata.firstName = ui->firstNamelineEdit->text();
+    mydata.lastName = ui->lastNamelineEdit->text();
+    mydata.address = ui->addresslineEdit->text();
+    mydata.city = ui->citylineEdit->text();
+    mydata.state = ui->statecomboBox->currentText();
+    mydata.zip = ui->ZIPlineEdit->text();
+    mydata.date = ui->dobdateEdit->date();
+//    doc.setHtml(mydata.firstName);
+//    ui->outputtextBrowser->setDocument(doc);
+
+}
+
+
+
+
--- krishnenduShekhar/Qt/helloWorld/mainwindow.h
+++ krishnenduShekhar/Qt/helloWorld/mainwindow.h
@@ -2,6 +2,21 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
+#include <QTextDocument>
+#include <QDate>
+#include <QStringList>
+#include <QMessageBox>
+
+typedef struct{
+    QString firstName;
+    QString lastName;
+    QString address;
+    QString city;
+    QString state;
+    QString zip;
+    QDate date;
+}DATA;
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +30,19 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+
+private slots:
+    void on_savePushButton_clicked();
+
+    void on_showPushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QStringList stateList = {"WA", "CA","OR"};
+    DATA mydata;
+//    QString outputString = "";
+    QTextDocument doc;
+    void gatherData();
 };
 
 #endif // MAINWINDOW_H
